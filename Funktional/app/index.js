40d62d72e5b5b037d4b7897228797c147aa80c02
Boris Turchik <hydralien@gmail.com>
2021-01-20 22:19:44 +0100

    Lock screen management

--- Funktional/app/index.js
+++ Funktional/app/index.js
@@ -6,6 +6,10 @@ import { me } from "appbit";
 import { today } from "user-activity";
 import { battery } from "power";
 import * as util from "../common/utils";
+import * as fs from "fs";
+
+const SETTINGS_TYPE = "cbor";
+const SETTINGS_FILE = "settings.cbor";
 
 let heartRate = 0;
 if (me.permissions.granted("access_heart_rate")) {
@@ -16,6 +20,8 @@ if (me.permissions.granted("access_heart_rate")) {
 	}
 	hrm.start();
 }
+
+import { minuteHistory, today } from "user-activity";
 
 // Update the clock every minute
 clock.granularity = "seconds";
@@ -36,9 +42,12 @@ const sMeter = document.getElementById("secondsCircle");
 
 const lockText = document.getElementById("lockText");
 const lockTextBg = document.getElementById("lockTextBg");
+const lockIcon = document.getElementById("lockIcon");
 const globalScape = document.getElementById("globalScape");
 
 const statsHeartRateText = document.getElementById("statsHeartRateText");
+const statsPaceText = document.getElementById("statsPaceText");
+const statsStepsText = document.getElementById("statsStepsText");
 
 const week = {
 	0: 'Sunday',
@@ -55,27 +64,78 @@ const screens = [
 	'stats'
 ];
 
-let screenIndex = 0;
+function loadSettings() {
+	try {
+		return fs.readFileSync(SETTINGS_FILE, SETTINGS_TYPE);
+	} catch (ex) {
+		return {};
+	}
+}
 
-let screenLocked = false;
+function saveSettings() {
+	fs.writeFileSync(SETTINGS_FILE, settings, SETTINGS_TYPE);
+}
+
+me.addEventListener("unload", saveSettings);
+
+let settings = loadSettings();
+
+const SCREEN_INDEX = 'screen-index';
+const LOCK_STATE = 'lock-state';
+let screenIndex = settings[SCREEN_INDEX] || 0;
+
+let screenLocked = settings[LOCK_STATE] || false;
+let lockTimer = undefined;
+let lockHintTimer = undefined;
 
 let hideLockHint = (evt) => {
 	lockText.style.visibility = 'hidden';
 	lockTextBg.style.visibility = 'hidden';
 }
 
+let lockToggle = (evt) => {
+	if (screenLocked) {
+		screenLocked = false;
+		hideLockHint(evt);
+	} else {
+		screenLocked = true;
+	}
+	lockIcon.style.visibility = screenLocked ? 'visible' : 'hidden';
+	settings[LOCK_STATE] = screenLocked;
+};
+
+globalScape.onload = () => {
+	console.log("ONLYLODE")
+	screens.forEach((screen, index) => {
+		const screenElement = document.getElementById(screen);
+		if (index === screenIndex) screenElement.style.visibility = 'visible'
+		else screenElement.style.visibility = 'hidden'
+	});
+	lockIcon.style.visibility = screenLocked ? 'visible' : 'hidden';
+};
+
 globalScape.onmousedown = (evt) => {
 	lockText.text = screenLocked ? "Long tap to unlock" : "Long tap to lock";
 	lockText.style.visibility = 'visible';
 	lockTextBg.style.visibility = 'visible';
-	setTimeout(hideLockHint, 2000)
-	if (screenLocked) return;
+	clearTimeout(lockHintTimer);
+	lockHintTimer = setTimeout(hideLockHint, 2000);
+	clearTimeout(lockTimer);
+	lockTimer = setTimeout(lockToggle, 2000);
+};
 
+globalScape.onmouseup = (evt) => {
+	clearTimeout(lockTimer);
+};
+
+globalScape.onclick = (evt) => {
+	if (screenLocked) return;
 	const prevScreen = document.getElementById(screens[screenIndex]);
 	screenIndex = (screenIndex + 1) % screens.length;
 	const newScreen = document.getElementById(screens[screenIndex]);
 	prevScreen.style.visibility = 'hidden';
 	newScreen.style.visibility = 'visible';
+	settings[SCREEN_INDEX] = screenIndex;
 }
 
 // Update the <text> element every tick with the current time
@@ -110,25 +170,35 @@ clock.ontick = (evt) => {
 	let monthNo = util.zeroPad(thisDay.getMonth()+1);
 	let yearNo = thisDay.getYear() + 1900;
 
-	statsHeartRateText.text = heartRate || '--';
+	// if (screens[screenIndex] === 'stats') {
+		statsHeartRateText.text = heartRate || '--';
+		if (me.permissions.granted("access_activity")) {
+			const minuteRecords = minuteHistory.query({ limit: 1 });
+			const lastMinute = minuteRecords[0];
+			// console.log("MIRO", minuteRecords.length, lastMinute.distance, lastMinute.steps);
+			statsStepsText.text = `Steps: ${today.local.steps}`;
+			statsPaceText.text = `Dist: ${lastMinute.distance}`;
+		}
+	// }
 
-	clockText.text = `${hours}:${mins}`;//:${secs}`;
-	dateText.text = `${yearNo}-${monthNo}-${dayNo}`;
-	weekText.text = week[thisDay.getDay()];
-	heartRateText.text = heartRate || '--';
-	floorsText.text = today.local ? (today.local.elevationGain || 0) : 0;
-	stepsText.text = today.local ? (today.local.steps || 0) : 0;
-	activeText.text = today.local ? (today.local.activeMinutes || 0) : 0;
-	//let batteryLevel = parseInt(battery.chargeLevel);
-	batteryText.text = `${battery.chargeLevel}%`;
+	// if (screens[screenIndex] === 'clock') {
+		clockText.text = `${hours}:${mins}`;//:${secs}`;
+		dateText.text = `${yearNo}-${monthNo}-${dayNo}`;
+		weekText.text = week[thisDay.getDay()];
+		heartRateText.text = heartRate || '--';
+		floorsText.text = today.local ? (today.local.elevationGain || 0) : 0;
+		stepsText.text = today.local ? (today.local.steps || 0) : 0;
+		activeText.text = today.local ? (today.local.activeMinutes || 0) : 0;
+		//let batteryLevel = parseInt(battery.chargeLevel);
+		batteryText.text = `${battery.chargeLevel}%`;
 
-	let sweepAngle = 10;
-	let startAngle = parseInt(360/60 * seconds) - parseInt(sweepAngle/2);
-	if (startAngle < 0) {
-		startAngle += 360;
-	}
+		let sweepAngle = 10;
+		let startAngle = parseInt(360 / 60 * seconds) - parseInt(sweepAngle / 2);
+		if (startAngle < 0) {
+			startAngle += 360;
+		}
 
-	sMeter.startAngle = startAngle;
-	sMeter.sweepAngle = sweepAngle;
-
+		sMeter.startAngle = startAngle;
+		sMeter.sweepAngle = sweepAngle;
+	// }
 }
